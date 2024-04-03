5ec58dc4c5d661c8e637cb5ba2dc091454faf4ea
Muhammed Ali AK <aliak7974@gmail.com>
2024-04-04 02:39:05 +0300

    Project v1.2

--- WinFormUI/Program.cs
+++ WinFormUI/Program.cs
@@ -17,6 +17,7 @@ namespace WinFormUI
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmGiris());
+
         }
     }
 }
